b781a97eb7806e4bbb0b29e5f341b3a88f2a32b4
Mercedes Egido <mercedes@huridocs.org>
2024-08-25 19:31:49 +0200

    back

--- api/slack/events.js
+++ api/slack/events.js
@@ -1,5 +1,4 @@
-export async function POST(request) {
-  const body = await request.body
-  console.log(body)
+export function POST(request) {
+  console.log(request)
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
